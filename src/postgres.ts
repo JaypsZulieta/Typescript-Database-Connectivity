75ffbbd0547b93697ae0acf7adc19bdd80ea3c8d
Jaypee Zulieta <jaypee.zulieta@lsu.edu.ph>
2024-07-23 13:25:34 +0800

    feat: Add implementation for PostgresPreparedStatement

--- src/postgres.ts
+++ src/postgres.ts
@@ -7,31 +7,66 @@ class PostgresRowData extends RowData {}
 class PostgresStatement implements Statement {
   constructor(private connectionPool: PGConnectionPool, private SQL: string) {}
 
+  protected getConnectionPool(): PGConnectionPool {
+    return this.connectionPool;
+  }
+
+  protected getSQL(): string {
+    return this.SQL;
+  }
+
   async execute(): Promise<RowData[]> {
     const results = await this.connectionPool.query(this.SQL);
     return results.rows.map((data) => new PostgresRowData(data));
   }
 }
 
-class PostgresPreparedStatement implements PreparedStatement {
-  setString(value: string): PreparedStatement {
-    throw new Error("Method not implemented.");
+class PostgresPreparedStatement extends PostgresStatement implements PreparedStatement {
+  private parameters: any[];
+
+  constructor(connectionPool: PGConnectionPool, SQL: string) {
+    super(connectionPool, PostgresPreparedStatement.transFrom(SQL));
+    this.parameters = [];
   }
 
-  setNumber(value: string): PreparedStatement {
-    throw new Error("Method not implemented.");
+  private static transFrom(SQL: string): string {
+    let properSQLString = "";
+    let parameterIndex = 0;
+
+    for (let index = 0; index < SQL.length; index++) {
+      if (SQL[index] == "?") {
+        parameterIndex++;
+        properSQLString += `$${parameterIndex}`;
+      } else properSQLString += SQL[index];
+    }
+    return properSQLString;
+  }
+
+  setString(value: string): PreparedStatement {
+    this.parameters.push(value);
+    return this;
+  }
+
+  setNumber(value: number): PreparedStatement {
+    this.parameters.push(value);
+    return this;
   }
 
   setBoolean(value: boolean): PreparedStatement {
-    throw new Error("Method not implemented.");
+    this.parameters.push(value);
+    return this;
   }
 
   setDate(value: Date): PreparedStatement {
-    throw new Error("Method not implemented.");
+    this.parameters.push(value);
+    return this;
   }
 
-  execute(): Promise<RowData[]> {
-    throw new Error("Method not implemented.");
+  async execute(): Promise<RowData[]> {
+    const connectionPool = this.getConnectionPool();
+    const SQL = this.getSQL();
+    const results = await connectionPool.query(SQL, this.parameters);
+    return results.rows.map((data) => new PostgresRowData(data));
   }
 }
 
@@ -47,6 +82,6 @@ export class PostgresConnection implements DatabaseConnection {
   }
 
   prepareStatement(SQL: string): PreparedStatement {
-    throw new Error("Method not implemented.");
+    return new PostgresPreparedStatement(this.connectionPool, SQL);
   }
 }
